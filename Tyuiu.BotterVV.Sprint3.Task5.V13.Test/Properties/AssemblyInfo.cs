74358a63ef6764fa95ebb44639f03015d5abf83f
Tyuiu.BotterVV@outlook.com <masya2012a@gmail.com>
2023-10-25 12:08:25 +0500

    Task5

---
+++ Tyuiu.BotterVV.Sprint3.Task5.V13.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.BotterVV.Sprint3.Task5.V13.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.BotterVV.Sprint3.Task5.V13.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("92805632-38d6-4213-9095-c6aff64a90ba")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
